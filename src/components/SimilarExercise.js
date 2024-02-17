de88f2b90f48327967b0dda7fe559101c22cf274
Gautam Sarawagi <101802666+gautamsarawagi@users.noreply.github.com>
2024-02-17 19:22:19 +0530

    Update SimilarExercise.js

--- src/components/SimilarExercise.js
+++ src/components/SimilarExercise.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography, Stack } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import HorizontalScrollBar from "./HorizontalScrollBar"
 
 function SimilarExercise({equipment,targetMuscles}) {
